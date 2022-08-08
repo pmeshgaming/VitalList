880aa5a7e7a693520008b9eff4b543197c593af4
Bryden Calaway <Brydenwcgames@gmail.com>
2022-08-08 00:20:15 -0500

    push

--- src/servers/client.js
+++ src/servers/client.js
@@ -5,5 +5,5 @@ const { EmbedBuilder } = require('discord.js');
 
 client.on('ready', () => {
     logger.system(`${client.user.tag} is online and ready.`)
-    client.user.setActivity("VitalServers")
+    client.user.setActivity("VitalServers.xyz")
 })
