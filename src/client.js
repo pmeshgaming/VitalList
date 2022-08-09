a89da8a7a70508b8e9ef50b1f5e71e9e35a6c5f0
Bryden Calaway <Brydenwcgames@gmail.com>
2022-08-09 15:32:22 -0500

    Added Button Roles

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 const client = global.client;
 const config = global.config;
 const logger = global.logger;
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require('discord.js');
 const { inspect } = require('util');
 
 client.on('ready', () => {
@@ -70,3 +70,56 @@ client.on("messageCreate", async (message) => {
     message.channel.bulkDelete(fetched)
  }
 })
+
+//-Button Roles-//
+client.on("interactionCreate", async (interaction) => {
+  const r1 = "1006653826843029654";
+  const r2 = "1006653433970958376";
+  const r3 = "1006653765920755825";
+
+  if (interaction.isButton()) {
+    if (interaction.customId == "r1") {
+      if (interaction.member.roles.cache.some((role) => role.id == r1)) {
+        interaction.reply({
+          content: `The role **Sneak Peaks** was successfully removed from you.`,
+          ephemeral: true,
+        });
+        interaction.member.roles.remove(r1);
+      } else {
+        interaction.member.roles.add(r1);
+        await interaction.reply({
+          content: `The role **Sneak Peaks** was successfully added to you.`,
+          ephemeral: true,
+        });
+      }
+    } else if (interaction.customId == "r2") {
+      if (interaction.member.roles.cache.some((role) => role.id == r2)) {
+        interaction.reply({
+          content: `The role **Announcements** was successfully removed from you!`,
+          ephemeral: true,
+        });
+        interaction.member.roles.remove(r2);
+      } else {
+        interaction.member.roles.add(r2);
+        await interaction.reply({
+          content: `The role **Announcements** was successfully added to you!`,
+          ephemeral: true,
+        });
+       }
+      } else if (interaction.customId == "r3") {
+        if (interaction.member.roles.cache.some((role) => role.id == r3)) {
+          interaction.reply({
+            content: `The role **Website Status** was successfully removed from you!`,
+            ephemeral: true,
+          });
+          interaction.member.roles.remove(r3);
+        } else {
+          interaction.member.roles.add(r3);
+          await interaction.reply({
+            content: `The role **Website Status** was successfully added to you!`,
+            ephemeral: true,
+          });
+        }
+    }
+  }
+});
