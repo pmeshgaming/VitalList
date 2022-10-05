0a37a335c37a338864fbf9a1c221c2907dc780bc
Bryden Calaway <Brydenwcgames@gmail.com>
2022-10-05 08:30:37 -0500

    Home page fixed with mobile friendly.

--- src/app.js
+++ src/app.js
@@ -166,6 +166,10 @@ app.get("/auth/logout", function(req, res) {
     });
 });
 
+/*app.get("/arc-sw.js", function(req, res) {
+    res.send('!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=93)}({3:function(t,e,n){"use strict";n.d(e,"a",(function(){return r})),n.d(e,"c",(function(){return o})),n.d(e,"g",(function(){return i})),n.d(e,"j",(function(){return a})),n.d(e,"i",(function(){return d})),n.d(e,"b",(function(){return f})),n.d(e,"k",(function(){return u})),n.d(e,"d",(function(){return p})),n.d(e,"e",(function(){return l})),n.d(e,"f",(function(){return m})),n.d(e,"h",(function(){return v}));var r={images:["bmp","jpeg","jpg","ttf","pict","svg","webp","eps","svgz","gif","png","ico","tif","tiff","bpg","avif","jxl"],video:["mp4","3gp","webm","mkv","flv","f4v","f4p","f4bogv","drc","avi","mov","qt","wmv","amv","mpg","mp2","mpeg","mpe","m2v","m4v","3g2","gifv","mpv","av1","ts","tsv","tsa","m2t","m3u8"],audio:["mid","midi","aac","aiff","flac","m4a","m4p","mp3","ogg","oga","mogg","opus","ra","rm","wav","webm","f4a","pat"],interchange:["json","yaml","xml","csv","toml","ini","bson","asn1","ubj"],archives:["jar","iso","tar","tgz","tbz2","tlz","gz","bz2","xz","lz","z","7z","apk","dmg","rar","lzma","txz","zip","zipx"],documents:["pdf","ps","doc","docx","ppt","pptx","xls","otf","xlsx"],other:["srt","swf"]},o=["js","cjs","mjs","css"],c="arc:",i={COMLINK_INIT:"".concat(c,"comlink:init"),NODE_ID:"".concat(c,":nodeId"),CLIENT_TEARDOWN:"".concat(c,"client:teardown"),CLIENT_TAB_ID:"".concat(c,"client:tabId"),CDN_CONFIG:"".concat(c,"cdn:config"),P2P_CLIENT_READY:"".concat(c,"cdn:ready"),STORED_FIDS:"".concat(c,"cdn:storedFids"),SW_HEALTH_CHECK:"".concat(c,"cdn:healthCheck"),WIDGET_CONFIG:"".concat(c,"widget:config"),WIDGET_INIT:"".concat(c,"widget:init"),WIDGET_UI_LOAD:"".concat(c,"widget:load"),BROKER_LOAD:"".concat(c,"broker:load"),RENDER_FILE:"".concat(c,"inlay:renderFile"),FILE_RENDERED:"".concat(c,"inlay:fileRendered")},a="serviceWorker",d="/".concat("shared-worker",".js"),f="/".concat("dedicated-worker",".js"),u="/".concat("arc-sw-core",".js"),s="".concat("arc-sw",".js"),p=("/".concat(s),"/".concat("arc-sw"),"arc-db"),l="key-val-store",m=2**17,v="".concat("https://warden.arc.io","/mailbox/propertySession");"".concat("https://warden.arc.io","/mailbox/transfers")},93:function(t,e,n){"use strict";n.r(e);var r=n(3);if("undefined"!=typeof ServiceWorkerGlobalScope){var o="https://arc.io"+r.k;importScripts(o)}else if("undefined"!=typeof SharedWorkerGlobalScope){var c="https://arc.io"+r.i;importScripts(c)}else if("undefined"!=typeof DedicatedWorkerGlobalScope){var i="https://arc.io"+r.b;importScripts(i)}}});', type = 'application/javascript');
+  });*/
+
 //-BotList-//
 
 app.get("/", checkMaintenance, async(req, res) => {
@@ -210,6 +214,72 @@ app.get("/", checkMaintenance, async(req, res) => {
         user: req.user || null
     });
 })
+app.get("/bots", checkMaintenance, async(req, res) => {
+    const client = global.client;
+
+    let model = require("./models/bot.js");
+    let bots = await model.find({
+        approved: true
+    });
+    let dbots = await model.find({
+        denied: false
+    });
+
+    for (dbot of dbots) {
+        const tendaysago = new Date().getTime() - 10 * 24 * 60 * 60 * 1000;
+        if (dbot.deniedOn < tendaysago) {
+            dbot.deleteOne()
+            dbot.save()
+        }
+    }
+
+    for (let i = 0; i < bots.length; i++) {
+        const BotRaw = await client.users.fetch(bots[i].id);
+        bots[i].name = BotRaw.username;
+        bots[i].avatar = BotRaw.avatar;
+        bots[i].name = bots[i].name.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+        bots[i].tags = bots[i].tags.join(", ")
+    }
+    Array.prototype.shuffle = function() {
+        let a = this;
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    };
+
+
+    res.render("botlist/bots.ejs", {
+        bot: req.bot,
+        bots: bots.shuffle(),
+        user: req.user || null
+    });
+})
+
+app.get("/bots/:id", checkMaintenance, async(req, res) => {
+    let id = req.params.id;
+    const client = global.client;
+    const model = require("./models/bot.js")
+    const bot = await model.findOne({ id: id});
+    if(!bot) return res.status(404).send("This bot was not found on our list.");
+
+    const BotRaw = (await client.users.fetch(id)) || null;
+    const OwnerRaw = client.users.fetch(bot.owner);
+    bot.name = BotRaw.username;
+    bot.avatar = BotRaw.avatar;
+    bot.discriminator = BotRaw.discriminator;
+    bot.tag = BotRaw.tag;
+    bot.ownerTag = OwnerRaw.tag;
+    bot.ownerAvatar = OwnerRaw.avatar;
+    bot.tags = bot.tags.join(", ")
+
+    res.render("botlist/viewbot.ejs", {
+        bot2: req.bot,
+        bot: bot,
+        user: req.user || null
+    });
+})
 
 app.get("/bots/new", checkMaintenance, checkAuth, async(req, res) => {
     res.render("botlist/add.ejs", {
@@ -230,12 +300,17 @@ app.post("/bots/new", checkMaintenance, checkAuth, async(req, res) => {
     }
 
     if (await model.findOne({
-            id: data.clientid
+            id: data.id
         })) return res.status(409).json({
         message: "This application has already been added to our site."
     });
 
     const bot = await client.users.fetch(data.id);
+    if(!bot) { 
+        return res.status(400).json({
+            message: "This is not a real application on Discord."
+        });
+    }
     await model
         .create({
             id: data.id,
@@ -253,7 +328,7 @@ app.post("/bots/new", checkMaintenance, checkAuth, async(req, res) => {
         });
     logs.send("<:VD_add:1006511788155752558> <@" + req.user.id + "> has submitted **" + bot.tag + "** to Vital List.")
 
-    res.redirect("/?=success")
+    return res.redirect("/?=success");
 
 })
 
@@ -444,9 +519,9 @@ app.post("/bots/:id/deny", checkAuth, checkStaff, async(req, res) => {
 app.use('/bots/:id/status', checkAuth, checkStaff, async(req, res) => {
     const client = global.client;
     const logs = client.channels.cache.get(config.channels.weblogs)
-    const BotRaw = await client.users.fetch(req.body.id);
+    const BotRaw = await client.users.fetch(req.params.id);
     let model = require("./models/bot.js");
-    let bot = await model.findOne({ id: req.body.id });
+    let bot = await model.findOne({ id: req.params.id });
 
     if (!bot) return res.status(404).json({
         message: "This application could not be found in our site."
@@ -544,8 +619,10 @@ function checkMaintenance(req, res, next) {
     if (!req.user) return res.render("errors/503.ejs", {
         user: req.user || null
     });
-    if (!config.staff.includes(req.user.id)) return res.render("errors/503.ejs", {
+    if (!config.betatesters.includes(req.user.id)) {  
+        return res.render("errors/503.ejs", {
         user: req.user || null
-    });
+    })
+} 
     return next();
 }
