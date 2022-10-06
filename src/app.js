a93d624414a4863b8ca075d7030d9e5464d98bb1
Bryden Calaway <Brydenwcgames@gmail.com>
2022-10-06 18:05:52 -0500

    bad

--- src/app.js
+++ src/app.js
@@ -257,30 +257,6 @@ app.get("/bots", checkMaintenance, async(req, res) => {
     });
 })
 
-app.get("/bots/:id", checkMaintenance, async(req, res) => {
-    let id = req.params.id;
-    const client = global.client;
-    const model = require("./models/bot.js")
-    const bot = await model.findOne({ id: id});
-    if(!bot) return res.status(404).send("This bot was not found on our list.");
-
-    const BotRaw = (await client.users.fetch(id)) || null;
-    const OwnerRaw = client.users.fetch(bot.owner);
-    bot.name = BotRaw.username;
-    bot.avatar = BotRaw.avatar;
-    bot.discriminator = BotRaw.discriminator;
-    bot.tag = BotRaw.tag;
-    bot.ownerTag = OwnerRaw.tag;
-    bot.ownerAvatar = OwnerRaw.avatar;
-    bot.tags = bot.tags.join(", ")
-
-    res.render("botlist/viewbot.ejs", {
-        bot2: req.bot,
-        bot: bot,
-        user: req.user || null
-    });
-})
-
 app.get("/bots/new", checkMaintenance, checkAuth, async(req, res) => {
     res.render("botlist/add.ejs", {
         bot: global.client,
@@ -330,6 +306,39 @@ app.post("/bots/new", checkMaintenance, checkAuth, async(req, res) => {
 
     return res.redirect("/?=success");
 
+})
+
+app.get("/bots/:id", checkMaintenance, async(req, res) => {
+    let id = req.params.id;
+    const client = global.client;
+    const model = require("./models/bot.js")
+    const bot = await model.findOne({ id: id});
+    const guild = await client.guilds.fetch(global.config.guilds.main)
+    if(!bot) return res.status(404).send("This bot was not found on our list.");
+
+    try { 
+        guild.members.fetch(id) || null;
+     } catch(err) {
+             return res.status(404).send("This bot is not in our Discord server, so we could not fetch it's data. Error: "+ err);
+     }
+
+    const BotRaw = (await client.users.fetch(id)) || null;
+    const OwnerRaw = client.users.fetch(bot.owner);
+    const PresenceRaw = await guild.members.fetch(id) || null;
+    bot.name = BotRaw.username;
+    bot.avatar = BotRaw.avatar;
+    bot.presence = PresenceRaw.presence;
+    bot.discriminator = BotRaw.discriminator;
+    bot.tag = BotRaw.tag;
+    bot.ownerTag = OwnerRaw.tag;
+    bot.ownerAvatar = OwnerRaw.avatar;
+    bot.tags = bot.tags.join(", ")
+
+    res.render("botlist/viewbot.ejs", {
+        bot2: req.bot,
+        bot: bot,
+        user: req.user || null
+    });
 })
 
 //-API-//
@@ -445,13 +454,12 @@ app.get("/queue", checkAuth, checkStaff, async(req, res) => {
     });
 
     for (let i = 0; i < inprogress.length; i++) {
-        console.log(inprogress[i])
         const IPRaw = await client.users.fetch(inprogress[i].id);
-        const reviewerRaw = await client.users.fetch("529815278456930314")
+        const ReviewerRaw = await client.users.fetch(inprogress[i].reviewer);
         inprogress[i].tag = IPRaw.tag;
         inprogress[i].name = IPRaw.username;
         inprogress[i].avatar = IPRaw.avatar;
-        inprogress[i].reviewer = reviewerRaw.tag;
+        inprogress[i].reviewer = ReviewerRaw.tag;
         inprogress[i].tags = inprogress[i].tags.join(", ")
     }
 
@@ -475,6 +483,11 @@ app.get("/queue", checkAuth, checkStaff, async(req, res) => {
 
 app.get("/bots/:id/approve", checkAuth, checkStaff, async(req, res) => {
     const config = global.config; 
+    let model = require("./models/bot.js");
+    let bot = await model.findOne({ id: req.params.id });
+    if (!bot) return res.status(404).json({
+        message: "This application could not be found in our site."
+    });
   
     res.render("queue/approve.ejs", {
       bot: req.bot,
@@ -486,7 +499,12 @@ app.get("/bots/:id/approve", checkAuth, checkStaff, async(req, res) => {
 
 app.get("/bots/:id/deny", checkAuth, checkStaff, async(req, res) => {
   const config = global.config; 
-
+  let model = require("./models/bot.js");
+  let bot = await model.findOne({ id: req.params.id });
+  if (!bot) return res.status(404).json({
+      message: "This application could not be found in our site."
+  });
+  
   res.render("queue/deny.ejs", {
     bot: req.bot,
     id: req.params.id,
@@ -511,6 +529,7 @@ app.post("/bots/:id/deny", checkAuth, checkStaff, async(req, res) => {
     bot.tag = BotRaw.tag;
     bot.denied = true;
     bot.tested = true;
+    bot.inprogress = false;
     bot.ownerName = OwnerRaw.tag;
     bot.reason = req.body.reason;
     bot.deniedOn = Date.now();
@@ -549,6 +568,7 @@ app.use('/bots/:id/status', checkAuth, checkStaff, async(req, res) => {
     if (req.method === 'POST') {
         bot.tag = BotRaw.tag;
         bot.approved = true
+        bot.inprogress = false;
         bot.ownerName = OwnerRaw.tag;
         bot.approvedOn = Date.now();
         bot.tested = true
