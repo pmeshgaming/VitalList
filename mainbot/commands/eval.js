87d72c1a8246825439590d0051085fb3ce15e181
sldless <67599596+sldless@users.noreply.github.com>
2022-11-06 20:34:01 -0500

    eval

--- mainbot/commands/eval.js
+++ mainbot/commands/eval.js
@@ -1,4 +1,6 @@
-const inspect = require("util").inspect;
+const { getCode, clean } = require("@elara-services/eval-helper");
+
+
 module.exports = {
   name: "eval",
   description: "Evaluates Javascript code in a command.",
@@ -8,28 +10,27 @@ module.exports = {
       return message.reply({
         content: `:x: You must provide a code to evaluate.`,
       });
-    let evaled;
-    try {
-      evaled = await eval(args.join(" "));
-      if (evaled == client.token)
-        return message.reply({
-          content: `:x: Stop trying to get my token, this is why your dad left.`,
-        });
-      if (typeof evaled !== "string") {
-        evaled = inspect(evaled);
+     const evaled = await getCode({
+        code: args.join(" "),
+        async: true, 
+      });
+      const code = await clean(eval(evaled), [
+        client.token
+    ]);
+    try { 
+      if (typeof code !== "string") {
         message.channel.send({
-          content: `:white_check_mark: Successfully evaluated.\n\`\`\`js\n${evaled}\n\`\`\``,
+          content: `:white_check_mark: Successfully evaluated.\n\`\`\`js\n${code}\n\`\`\``,
         });
       } else {
         message.channel.send({
-          content: `:white_check_mark: Successfully evaluated.\n\`\`\`js\n${evaled}\n\`\`\``,
+          content: `:white_check_mark: Successfully evaluated.\n\`\`\`js\n${code}\n\`\`\``,
         });
       }
-    } catch (error) {
-      console.error(error);
-      message.reply({ content: `:x: There was an error during evaluation.` });
+    } catch (e) {
+      message.channel.send({
+        content: ":x: There was an error during evaluation."
+      })
     }
   },
 };
-
-// use https://npmjs.com/package/@elara-services/eval-helper , this is just no
